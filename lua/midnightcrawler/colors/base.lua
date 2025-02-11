baf9c252f24d794500a4ac1d18a36e5b6fb9cf8a
Jakub <zuber.kuba@gmail.com>
2025-02-11 14:38:38 +0100

    wip(base): light + dark

--- lua/midnightcrawler/colors/base.lua
+++ lua/midnightcrawler/colors/base.lua
@@ -2,40 +2,49 @@ local M = {}
 
 M.foreground = '#ededed'
 M.fg = M.foreground
+M.cursorColor = M.foreground
+
 M.background = '#000000'
 M.bg = M.background
-M.cursorColor = M.foreground
 
 M.color0 = '#0d1016' -- StatusLine
 M.color8 = '#404755' -- CursorLine
-M.black = M.color0
+M.black_dark = M.color0
+M.black_light = M.color8
 
 M.color1 = '#ff3333'
 M.color9 = '#ec3960'
-M.red = M.color9
+M.red_dark = M.color1
+M.red_light = M.color9
 
 M.color2 = '#0c9406'
 M.color10 = '#8af478'
-M.green = M.color10
+M.green_dark = M.color2
+M.green_light = M.color10
 
 M.color3 = '#ff8200'
 M.color11 = '#ffc400'
-M.yellow = M.color11
+M.yellow_dark = M.color3
+M.yellow_light = M.color11
 
 M.color4 = '#0017ff'
 M.color12 = '#3388f8'
-M.blue = M.color12
+M.blue_dark = M.color4
+M.blue_light = M.color12
 
 M.color5 = '#a500ab'
 M.color13 = '#c400ff'
-M.magenta = color5
+M.magenta_dark = color5
+M.magenta_light = color13
 
 M.color6 = '#8abeb7'
 M.color14 = '#15f1d5'
-M.cyan = M.color14
+M.cyan_dark = M.color6
+M.cyan_light = M.color14
 
 M.color7 = '#b3b1ad'
 M.color15 = '#ffffff'
-M.white = M.color7
+M.white_dark = M.color7
+M.white_light = M.color15
 
 return M
