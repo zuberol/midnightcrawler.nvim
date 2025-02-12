c89f5451a1ee3b06bf7d9bd9f496275d6557112e
Jakub <zuber.kuba@gmail.com>
2025-02-12 10:27:41 +0100

    add(groups): msg, search, spell, special, panels

--- lua/midnightcrawler/groups.lua
+++ lua/midnightcrawler/groups.lua
@@ -13,18 +13,31 @@ local custom = {
 
 local panels = {
 	Normal = { fg = base.white_dark, bg = none },
+	NormalNC = { link = 'Normal' },
 	-- Normal = { fg = base.yellow_dark, bg = none }
 	NormalFloat = custom.PanelActive,
+	FloatBorder = { fg = none, bg = none },
+	FloatTitle = { fg = base.fg, bg = none, bold = true },
+	FloatFooter = { fg = base.fg, bg = none, bold = true },
 	ColorColumn = custom.None,
 	CursorLine = PanelActive,
 	StatusLine = custom.None,
 	StatusLineNC = custom.None,
 	TabLine = custom.None,
 	TabLineSel = PanelActiveSel,
+	TabLineFill = custom.None,
 	Pmenu = custom.PanelActive,
+	PmenuKind = { link = 'Pmenu' },
 	PmenuSel = custom.PanelActiveSel,
+	PmenuKindSel = { link = 'PmenuSel' },
+	PmenuExtra = { link = 'Pmenu' },
+	PmenuExtraSel = { link = 'Pmenu' },
+	PmenuSbar = { fg = none, bg = base.black_dark },
+	PmenuThumb = { fg = none, bg = base.white_dark },
 	WinSeparator = { fg = base.bg, bg = base.bg },
 	WildMenu = custom.PanelActiveSel,
+	WinBar = custom.None,
+	WinBarNC = custom.None,
 }
 
 local cursor = {
@@ -35,10 +48,21 @@ local cursor = {
 	TermCursorNC = { link = 'Cursor' },
 }
 
+local spell = {
+	-- SpellBad	
+	-- SpellCap	
+	-- SpellLocal	
+	-- SpellRare	
+}
+
 local special = {
 	NonText = { fg = base.magenta_dark, bg = none },
 	Special = { fg = base.yellow_light, bg = none },
 	EndOfBuffer = { fg = base.magenta_dark, bg = none },
+	Conceal = { fg = base.green_light, bg = none },
+	Folded = { fg = base.cyan_light, bg = none },
+	SpecialKey = { fg = base.magenta_light, bg = none },
+	Whitespace = { fg = base.magenta_dark, bg = none },
 }
 
 local diff = {
@@ -59,6 +83,9 @@ local search = {
 	IncSearch	= { fg = base.black, bg = base.cyan_light },
 	Substitute	= { fg = base.black, bg = base.green_light },
 	Search	= { fg = base.black, bg = base.green_light },
+	MatchParen = { fg = base.black, bg = base.yellow_light },
+	Visual = { fg = base.black, bg = base.cyan_light },
+	VisualNOS = { link = 'Visual' },
 }
 
 local syntax = {
@@ -68,7 +95,7 @@ local syntax = {
 	Type = { fg = base.blue_light, bg = none },
 	Variable = { fg = base.white_light, bg = none },
 	String = { fg = base.green_dark, bg = none },
---	Function = { link = 'Type' },
+	Function = { link = 'Type' },
 	['@lsp.mod.definition'] = { link = 'Type' },
 	['@lsp.mod.readonly'] = { fg = base.magenta_dark, bg = none },
 	['@lsp.type.namespace'] = { link = 'Type' },
@@ -78,16 +105,25 @@ local syntax = {
 	['@lsp.type.number'] = { fg = base.magenta_light, bg = none },
 	['@lsp.typemod.variable.definition'] = { fg = base.red_dark, bg = none },
 	['@lsp.typemod.parameter.definition'] = { fg = base.red_dark, bg = none },
---	['@lsp.typemod.function.definition'] = { fg = base.blue_light, bg = none },
-	-- ['@lsp'] = { fg = base.cyan_light, bg = none },
+	SnippetTabstop = { fg = base.magenta_light, bg = none },
 }
 
-local misc = {
+local msg = {
 	ErrorMsg = { fg = base.red_dark, bg = none },
 	WarningMsg = { fg = base.yellow_dark, bg = none },
-
-	Conceal = { fg = base.green_light, bg = none }, -- ??
-	Folded = { fg = base.cyan_light, bg = none },
+	ModeMsg = { link = 'Normal' },
+	MsgArea = { fg = none, bg = none },
+	MoreMsg = { link = 'Normal' },
+	QuickFixLine = { fg = base.cyan_light, bg = none },
+	Question = { fg = base.cyan_light, bg = none },
+	Title = { fg = base.fg, bg = none, bold = true },
 }
 
-return vim.tbl_deep_extend('error', {}, panels, cursor, special, diff, linenr, search, syntax, misc)
+local nvim = {
+	Identifier = { link = 'Keyword' },
+	Delimiter = { link = 'Operator' },
+	['@variable'] = { link = 'Normal' },
+	['@property'] = { link = '@variable' },
+}
+
+return vim.tbl_deep_extend('error', {}, panels, cursor, special, diff, linenr, search, syntax, msg, nvim)
